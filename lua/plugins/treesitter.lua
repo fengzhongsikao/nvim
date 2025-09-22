d80cfeaa46087f7bb23afc320d12bad837c98c6d
five <2974796278@@qq.com>
2025-09-22 21:09:48 +0800

    change:astronvim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,15 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
-      "go",
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
     },
   },
 }
